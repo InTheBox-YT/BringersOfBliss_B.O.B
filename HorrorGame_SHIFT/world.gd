9a47fb81bf76d24ff3349b572d326883609526d6
Jaycob2464 <pedersenjaycob@gmail.com>
2024-08-08 21:05:53 +1200

    e

--- HorrorGame_SHIFT/world.gd
+++ HorrorGame_SHIFT/world.gd
@@ -20,6 +20,8 @@ func _on_host_button_pressed():
 	multiplayer.peer_disconnected.connect(remove_player)
 	
 	add_player(multiplayer.get_unique_id())
+	
+	upnp_setup()
 
 func _on_join_button_pressed():
 	main_menu.hide()
@@ -33,3 +35,23 @@ func add_player(peer_id):
 
 func remove_player(peer_id):
 	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+
+func upnp_setup():
+	var upnp = UPNP.new()
+	
+	var discover_result = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Discover Failed! Error %s" % discover_result)
+	
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
+		"UPNP Invalid Gateway!")
+	
+	var map_result = upnp.add_port_mapping(PORT)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Port Mapping Failed! Error %s" % map_result)
+	
+	print("Success! Join Address: %s" % upnp.query_external_address())
+	
+	
