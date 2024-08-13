186078a36cc5d3ba52fe68b769b830822a1614a5
InTheBox-YT <intheboxyt@gmail.com>
2024-08-13 20:18:29 +1200

    sprint

--- HorrorGame_SHIFT/Sprites/Player.gd
+++ HorrorGame_SHIFT/Sprites/Player.gd
@@ -3,14 +3,21 @@ extends CharacterBody3D
 
 # Base Variables
 # I WANNA KILL MYSLEF
-const SPEED = 6.0
+const WALK_SPEED = 6.0
+const RUN_SPEED = 8.0
+const CROUCH_SPEED = 3.0
+var STAMINA = 20
+
 const JUMP_VELOCITY = 5.0
 const GRAVITY = 25.0
 
+var curspeed = WALK_SPEED
 var normal_fov = 70.0
 var zoomed_fov = 30.0
 var zoom_speed = 5.0
 var target_fov = normal_fov
+
+
 
 func _enter_tree():
 	set_multiplayer_authority(str(name).to_int())
@@ -34,6 +41,8 @@ func _unhandled_input(event):
 		
 
 func _physics_process(delta):
+	print(STAMINA)
+	
 	if not is_multiplayer_authority(): return
 	
 	# Add the gravity.
@@ -43,16 +52,30 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	# Adds Running
+	if Input.is_action_pressed("Run") and is_on_floor():
+		STAMINA -= delta;
+		if STAMINA >= 0:
+			curspeed = RUN_SPEED
+		else:
+			curspeed = WALK_SPEED
+	else:
+		if STAMINA <= 20:
+			STAMINA += delta
+		else:
+			pass
+		curspeed = WALK_SPEED
+	
 	# Handle movement.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * curspeed
+		velocity.z = direction.z * curspeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, curspeed)
+		velocity.z = move_toward(velocity.z, 0, curspeed)
 
 	move_and_slide()
 
