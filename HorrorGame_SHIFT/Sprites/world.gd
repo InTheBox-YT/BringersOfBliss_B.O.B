7fde571fbb8cd374d1010503a90d42ed79928195
Jaycob2464 <pedersenjaycob@gmail.com>
2024-08-14 21:00:11 +1200

    yes(may Override)

--- HorrorGame_SHIFT/Sprites/world.gd
+++ HorrorGame_SHIFT/Sprites/world.gd
@@ -8,10 +8,7 @@ extends Node3D
 var playerInstance = Player.instantiate()
 #var shader = playerInstance.get_node("$CanvasLayer/TextureRect")
 #var camera = playerInstance.get_node("$Camera3D")
-
 const Player = preload("res://Scenes/Player.tscn") 
-const PORT = 6969
-var enet_peer = ENetMultiplayerPeer.new()
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("Pause"):
@@ -25,21 +22,12 @@ func _on_quit_button_pressed():
 func _on_host_button_pressed():
 	main_menu.hide()
 	pause_menu.hide()
-	
-	enet_peer.create_server(PORT)
-	multiplayer.multiplayer_peer = enet_peer
-	multiplayer.peer_connected.connect(add_player)
-	multiplayer.peer_disconnected.connect(remove_player)
-	
 	add_player(multiplayer.get_unique_id())
-	
-	#upnp_setup()
 
 func _on_join_button_pressed():
 	main_menu.hide()
 	
-	enet_peer.create_client("localhost", PORT)
-	multiplayer.multiplayer_peer = enet_peer
+
 func add_player(peer_id):
 	var player = Player.instantiate()
 	player.name = str(peer_id)
@@ -47,28 +35,7 @@ func add_player(peer_id):
 
 func remove_player(peer_id):
 	var player = get_node_or_null(str(peer_id))
-	if player:
-		player.queue_free()
-
-func upnp_setup():
-	var upnp = UPNP.new()
 	
-	var discover_result = upnp.discover()
-	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
-		"UPNP Discover Failed! Error %s" % discover_result)
-	
-	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
-		"UPNP Invalid Gateway!")
-	
-	var map_result = upnp.add_port_mapping(PORT)
-	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
-		"UPNP Port Mapping Failed! Error %s" % map_result)
-	
-	print("Success! Join Address: %s" % upnp.query_external_address())
-	
-	
-
-
 func _on_back_button_pressed():
 	pause_menu.hide()
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
